ae56d00083000c52a0c49c0e15d64182176b9f88
ahmed saadawi <f_22w3@hotmail.com>
2020-02-23 19:23:50 +0400

    ..

--- js.js
+++ js.js
@@ -3,3 +3,20 @@
         alert( "called." );
         event.preventDefault();
       });
+
+      $('.carousel[data-type="multi"] .item').each(function() {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+    
+        for (var i = 0; i < 5; i++) {
+            next = next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+    
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
